18e39748df927652fff4d61350160c2fb12ca7ec
Nathan <nathanlynes123@gmail.com>
2024-03-05 15:40:45 -0500

    Adds context menu item; fetches highlighted text.

--- src/background.js
+++ src/background.js
@@ -6,5 +6,21 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
  console.log(request);
 });
 
+// Create a new selection within the context menu
+// Currently, prints grabbed text to the console
+browser.menus.create( 
+    {
+        id: "log-selection",
+        title: "Log % to console",
+        contexts: ["selection"],
+    }
+);
+
+browser.menus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId == "log-selection") {
+        console.log(info.selectionText);
+    }
+})
+
 
 
