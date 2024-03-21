2fac5a1723c6e6983541e0bb75bfa15f27ae2993
JGM01 <jacobgermanamccray@gmail.com>
2024-03-21 14:43:39 -0400

    trying to make the thing look better

--- src/content.js
+++ src/content.js
@@ -1,15 +1,23 @@
 const API_URL = "https://api.openai.com/v1/chat/completions";
 
+function injectFontAwesome() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css';
+    document.head.appendChild(link);
+}
+
 browser.runtime.onMessage.addListener((request) => {
 	switch (request.action) {
 		case 'summarize':
+			injectFontAwesome();
 			displaySummaryContainer(request.selectedText);
 			break;
 		case 'setResultText':
-			document.getElementById("bb-result-text").innerText = request.resultText;
+			document.getElementById("bot-messages").innerText = request.resultText;
 			break;
 		case 'updateResultText':
-			document.getElementById("bb-result-text").innerText += request.resultText;
+			document.getElementById("bot-messages").innerText += request.resultText;
 			break;
 
 		default:
@@ -39,10 +47,10 @@ function displaySummaryContainer(text) {
 
 function buildPromptContainer() {
 	const promptContainer = createElement('div', 'bb-prompt-container');
-	const header = createHeader();
+	//const header = createHeader();
 	const prompt = createPrompt();
 
-	promptContainer.append(header, prompt);
+	promptContainer.append(/*header, */prompt);
 	return promptContainer;
 }
 
@@ -84,6 +92,131 @@ function sendMessageToExtension(text) {
   });
 }
 
+function buildSummaryContainer() {
+	const summaryContainer = createElement('div', 'summary-container');
+	const headerContainer = createHeaderContainer();
+	const botContainer = createBotContainer();
+	const footerContainer = createFooterContainer();
+	
+	summaryContainer.append(headerContainer, botContainer, footerContainer);
+	return summaryContainer;
+}
+
+function createHeaderContainer() {
+	const headerContainer = createElement('div', 'header-container');
+
+	const headerTitleContainer = createElement('div', 'header-title-container');
+	const headerTitle = createElement('span', 'header-text');
+	headerTitle.innerText = 'Browser Buddy';
+	headerTitleContainer.append(headerTitle);
+	
+	const actionBtnContainer = createElement('div', 'action-btn-container');
+	
+//	const exitBtnIcon = createElementWithClass('i', 'fa fa-close');
+//	exitBtn.append(exitBtnIcon);
+
+	const exitBtn = createElement('button', 'exit-btn');
+
+	//const retryBtnIcon = createElementWithClass('i', 'fa fa-repeat');
+	//retryBtn.appendChild(retryBtnIcon);
+
+	const exitBtnIcon = document.createElement('i');
+	exitBtnIcon.classList.add('fa');
+	exitBtnIcon.classList.add('fa-close')
+
+	exitBtn.appendChild(exitBtnIcon);
+
+	exitBtn.addEventListener('click', () => {
+		console.log('exit button clicked')
+	});
+	actionBtnContainer.append(exitBtn);
+
+	headerContainer.append(headerTitleContainer);
+	headerContainer.append(actionBtnContainer);
+
+	return headerContainer;
+}
+
+function createBotContainer () {
+	const botContainer = createElement('div', 'bot-container');
+	const botMessages = createElement('div', 'bot-messages');
+	botContainer.append(botMessages);
+
+	return botContainer;
+}
+
+function createFooterContainer() {
+	const footerContainer = createElement('div', 'footer-container');
+	
+	const retryBtn = createElement('button', 'retry-btn');
+
+	retryBtn.innerText = 'Retry ';
+
+	//const retryBtnIcon = createElementWithClass('i', 'fa fa-repeat');
+	//retryBtn.appendChild(retryBtnIcon);
+
+	const retryBtnIcon = document.createElement('i');
+	retryBtnIcon.classList.add('fa');
+	retryBtnIcon.classList.add('fa-repeat')
+
+	retryBtn.appendChild(retryBtnIcon);
+
+	retryBtn.addEventListener('click', () => {
+		console.log('retry button clicked')
+	});
+	
+	const copyBtn = createElement('button', 'copy-btn');
+
+	copyBtn.innerText = 'Copy ';
+
+	//const retryBtnIcon = createElementWithClass('i', 'fa fa-repeat');
+	//retryBtn.appendChild(retryBtnIcon);
+
+	const copyBtnIcon = document.createElement('i');
+	copyBtnIcon.classList.add('fa');
+	copyBtnIcon.classList.add('fa-copy')
+
+	copyBtn.appendChild(copyBtnIcon);
+
+	copyBtn.addEventListener('click', () => {
+		console.log('copy button clicked')
+	});
+
+	footerContainer.append(retryBtn);
+	footerContainer.append(copyBtn);
+
+	return footerContainer;
+
+}
+
+function createButton(id, txt, icon, clickFunction) {
+
+	const btn = createElement('button', id);
+	btn.innerText = txt;
+	const btnIcon = document.createElement('i');
+	btnIcon.classList.add('fa');
+	btnIcon.classList.add(icon)
+
+	btn.append(btnIcon);
+	
+	btn.addEventListener('click', clickFunction);
+}
+
+//function createElementWithClass(tagName, className) {
+//	const element = document.createElement(tagName);
+//	if (className) element.classList.add(className);
+//	return element;
+//}
+
+function createElement(tagName, id) {
+  	const element = document.createElement(tagName);
+  	if (id) element.id = id;
+  	return element;
+}
+
+
+// OLD STUFF, KEEP IT FOR NOW BUT YKNOW
+/*
 function buildSummaryContainer() {
 	const summaryContainer = createElement('div', 'bb-summary-container');
 	const header = createHeader();
@@ -139,3 +272,4 @@ function createFooter(resultContainer) {
   	footer.append(retryBtn, copyBtn);
   	return footer;
 }
+*/
